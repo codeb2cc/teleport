b18e8ed11ced4514de57369c0b4d1524e8c07b9b
Codeb Fan <codeb2cc@gmail.com>
2012-12-29 21:12:33 +0800

    Add UDP server

---
+++ udp_server.py
@@ -0,0 +1,46 @@
+# -*- coding:utf-8 -*-
+
+import traceback
+
+from gevent.server import DatagramServer
+
+from teleport.db import db
+
+
+class PingServer(DatagramServer):
+    def handle(self, data, address):
+        try:
+            _raw = data.split('\t')
+
+            _token = _raw[0]
+            _ip = address[0]
+            _message = _raw[1]
+
+            gate = db['gate'].find_one({ 'token': _token })
+
+            if not gate:
+                self.socket.sendto('Error', address)
+
+            gate['counter'] += 1
+            len(gate['records']) >= 5 and gate['records'].pop()
+            gate['records'].insert(0, {
+                    'ip'     : _ip,
+                    'message': _message[:MESSAGE_MAX],
+                    'date'   : datetime.datetime.utcnow()
+                })
+
+            _id = db['gate'].save(gate, safe=True)
+
+
+            self.socket.sendto('OK', address)
+        except Exception:
+            traceback.print_exc()
+            self.socket.sendto('Error', address)
+
+
+def run_server(port):
+    try:
+        PingServer(':%d' % port).serve_forever()
+    except Exception:
+        traceback.print_exc()
+
